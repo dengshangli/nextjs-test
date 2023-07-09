bb155ce7e678606770c68951f3565f316f4b9cc5
dengshangli <544037207@qq.com>
2023-07-09 20:46:46 +0800

    refactor: 页面修改

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
         </p>
         <div>{`当前环境:${process.env.NODE_ENV}`}</div>
         <div>{`当前api:${process.env.PUBLIC_NEXT_API}`}</div>
+        <div>{`当前分支:test`}</div>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
